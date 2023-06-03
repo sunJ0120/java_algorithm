4c3d338dd83891f6f532e3d9ab3e675e435f9401
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-03 21:31:36 +0900

    자바 알고리즘 평균구하기 답
    내가 했던 방식과 비슷하나, 수식을 짧게 정리하여 가독성을 높이고 변수 생성 갯수를 줄였다는 장점이 있음

---
+++ src/배열과리스트001/answer_P1546_평균구하기.java
@@ -0,0 +1,25 @@
+package 배열과리스트001;
+import java.util.Scanner;
+
+public class answer_P1546_평균구하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int A[] = new int[N];
+		for(int i = 0; i< N; i++) {
+			A[i] = sc.nextInt();
+		}
+		long sum = 0;
+		long max = 0;
+		for(int i = 0; i< N; i++) {
+			if(A[i] > max) max = A[i];
+			sum += A[i];
+		}
+		
+		//한 과목과 관련된 수식을 총합한 후 관련된 수식으로 변환해 로직이 간단해짐
+		System.out.println(sum * 100.0/max/N);
+
+	}
+
+}
