d5783250ac49318783be004b5b08c19d91d7120e
BOOK-S08D1UN6CT\sspure1214 <sspure1214@naver.com>
2023-06-21 19:09:13 +0900

    005 나머지 합 구하기
    백준 10986번 골드 3단계 문제(실패)
    
    인덱스 부분 문제 없음

--- src/배열과리스트001/구간합구하기/백준_10986_도전.java
+++ src/배열과리스트001/구간합구하기/백준_10986_도전.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/**
+ * 검사해보니, list는 내가 i <= N+1이라고 포함해서 그렇지
+ * (index가 하나씩 더 작다는걸 망각함)
+ * 인덱스 설정 자체에 대한 문제는 없다.
+ */
+
 public class 백준_10986_도전 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,13 +20,13 @@ public class 백준_10986_도전 {
             listSum[i] = m;
         }
 
-        for(int i = 0; i < N; i++){
-            listSum[i+1] = listSum[i] + listSum[i+1];
+        for(int i = 1; i < N+1; i++){
+            listSum[i] = listSum[i] + listSum[i-1];
         }
 
-        for(int i = 0; i < N; i++){
-            listSum[i+1] %= M;
-            if((listSum[i+1] % M) == 0){
+        for(int i = 1; i < N+1; i++){
+            listSum[i] %= M;
+            if((listSum[i] % M) == 0){
                 answer += 1;
             }
         }
